929730487495cedc1baad1491d9f343c37c10570
Liam Dowd <liam.jonathan.dowd@gmail.com>
2025-12-04 22:04:56 +0000

    Adding different ways to creat output that I know

--- src/fundamentals/HelloWorld.java
+++ src/fundamentals/HelloWorld.java
@@ -1,8 +1,11 @@
 package fundamentals;
 
+import javax.swing.JOptionPane;
+
 public class HelloWorld{
     public static void main(String[] args){
-        System.out.println("Hello, World!");
-
+        System.out.println("This is one way to say Hello, World!");
+        JOptionPane.showMessageDialog(null, "This is another, but more in your face!" + "\nHello, World!");
+        JOptionPane.showConfirmDialog(null, "Are you watching?");
     }
 }
