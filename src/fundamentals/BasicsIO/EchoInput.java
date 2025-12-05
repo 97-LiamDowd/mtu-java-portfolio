d08319455037629a56184e5322c2175b7acf45fa
Liam Dowd <liam.jonathan.dowd@gmail.com>
2025-12-05 22:49:24 +0000

    Updated

--- src/fundamentals/BasicsIO/EchoInput.java
+++ src/fundamentals/BasicsIO/EchoInput.java
@@ -1,1 +1,38 @@
+package fundamentals.BasicsIO;
 
+import java.util.Scanner;
+
+public class EchoInput{
+    public static void main(String[] args){
+
+        String name;
+        int age;
+        double height;
+        boolean student;
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.println("What's your name?");
+        name = scanner.nextLine();
+        
+        System.out.println("What age are you?");
+        age = scanner.nextInt();
+        
+        System.out.println("What height are you?");
+        height = scanner.nextDouble();
+        
+        System.out.println("Are you a student? (True/False)");
+        student = scanner.nextBoolean();
+
+        
+        
+        
+
+
+        
+
+        scanner.close();
+
+    }
+
+}
