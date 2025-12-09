70529c00821e18922c2ea3d252a5acfdee5af72c
97-LiamDowd <liam.jonathan.dowd@gmail.com>
2025-12-09 01:30:43 +0000

    Almost finished -issue remaining is fixing 0, learning & fixing return and learning & fixing close

--- src/fundamentals/BasicsIO/SimpleCalculator.java
+++ src/fundamentals/BasicsIO/SimpleCalculator.java
@@ -15,24 +15,48 @@ public class SimpleCalculator{
 
         //Last updated: create each operator as such - String name = "Symbol"
         String add = "+";
+        String subtract = "-";
+        String multiply = "*";
+        String divide = "/";
 
+        //first user input
         System.out.print("Choose a number = ");
         num1 = scanner.nextInt();
 
-        //add = +, subtract = -, multiply = *, divide = / 
-        System.out.print("Choose between " + add + " ");
+        //add = +, subtract = -, multiply = *, divide = /
+        //prompting user input an operator
+        System.out.print("Choose between " + add + " " + subtract + " " + multiply + " " + divide + " = ");
         operator = scanner.next();
 
+        //second user input
         System.out.print("Choose another number = ");
         num2 = scanner.nextInt();
 
         //last update = use operator.equals(add/subtract/multiply/divide)
+
+        //outcome add
         if(operator.equals(add)){
             sum = num1 + num2;
-            System.out.print(sum);
+
+        //outcome subtract
+        }   else if(operator.equals(subtract)){
+                sum = num1 - num2;
+
+        //outcome multiply
+        }   else if(operator.equals(multiply)){
+                sum = num1 * num2;
+
+        //outcome divide
+        }   else if(operator.equals(divide)){
+                sum = num1 / num2;
+
+        }   else{
+            System.out.print("Invalid input");
+            return;
         }
-
-
+        
+        System.out.print(sum);
+        
         scanner.close();
 
     }//end of main method
